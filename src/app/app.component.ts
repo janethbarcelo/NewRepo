a2234a57dad26fc81d359cedee32bd033cc4bb5e
jane <janeth_barcelo@hotmail.com>
2023-03-08 13:38:21 -0600

    angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'janeth';
+  age = 30;
+  img = 'https://img.freepik.com/foto-gratis/primer-disparo-flor-morada_181624-25863.jpg?w=2000';
+  btnDisabled = true;
+  person = {
+  name: 'janeth',
+  age: '30',
+  avatar:'https://img.freepik.com/foto-gratis/primer-disparo-flor-morada_181624-25863.jpg?w=2000'
+}
+
+
+toggleButton  () {
+   this.btnDisabled= !this.btnDisabled;
+}
+
+increaseAge() {
+  this.person.age += 1;
+}
+}
+
---
+++ src/app/recap.ts
@@ -0,0 +1,20 @@
+const username: string |number = "janeth";
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+sum (2,5);
+
+class person {
+  age: number;
+  lastName: string;
+
+
+constructor(age: number, lastName: string){
+  this.age = age;
+  this.lastName = lastName;
+}
+
+  }
+    const nico = new person (15, "molina");
+    nico.age;
