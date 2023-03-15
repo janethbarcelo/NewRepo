558591de89b668fa5dec2db6ce489eced545d989
jane <janeth_barcelo@hotmail.com>
2023-03-15 15:48:51 -0600

    15 de marzo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {product} from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,6 +17,42 @@ export class AppComponent {
   age: 30,
   avatar:'https://img.freepik.com/foto-gratis/primer-disparo-flor-morada_181624-25863.jpg?w=2000'
 }
+names:string[] = ['nicolas', 'julio','santi'];
+newName = '';
+products:product[] = [
+  {
+    name:'el mejor juguete',
+    price:565,
+    image: './assets/images/toy.jpg',
+    category:'all'
+},
+{
+  name:'La mejor bici',
+  price:1245,
+  image: './assets/images/bici.jpg'
+},
+{
+  name:'Collecion de albumnes',
+  price:800,
+  image: './assets/images/collection album.jpg'
+},
+{
+  name:'Mis libros',
+  price:252,
+  image: './assets/images/libros.jpg'
+},
+{
+  name:'Casita',
+  price:650,
+  image: './assets/images/house.jpg'
+},
+{
+  name:'Gafas',
+  price:400,
+  image: './assets/images/glasses.jpg'
+},
+
+]
 
 
 toggleButton  () {
@@ -34,5 +72,16 @@ changeName(event:Event) {
   const element = event.target as HTMLInputElement;
   this.person.name = element.value;
 }
+
+addName(){
+  this.names.push(this.newName);
+  this.newName = '';
 }
+
+deleteName(index: number){
+  this.names.splice(index, 1);
+}
+
+}
+
 
