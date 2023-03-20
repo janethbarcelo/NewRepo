28b18edc3b9359d27ff6de79b2af71dbe45cbc7c
jane <janeth_barcelo@hotmail.com>
2023-03-20 13:34:07 -0600

    20 de marzo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {product} from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'janeth';
